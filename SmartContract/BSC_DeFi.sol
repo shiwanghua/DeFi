aea8f75c64d1faeb75155cc384dbe6e731db7614
shiwanghua <2804324275@qq.com>
2020-12-13 21:06:31 +0800

    add a demo contract to call contract on test BSC chain

--- SmartContract/BSC_DeFi.sol
+++ SmartContract/BSC_DeFi.sol
@@ -23,14 +23,14 @@ interface BSCswapRouter{
 // CheeseSwap 0x3047799262d8D2EF41eD2a222205968bC9B0d895
 interface CheeseSwapRouter{
      function getAmountsOut(uint amountIn, address[] calldata path) external returns (uint[] memory amounts);
-    
+
 }
 
 // interface hello_original {
 //     function get_z() view external returns (uint);
 //     function set_z(uint) external returns (uint);
 // }
-// remix 本合约地址：
+// 本合约在BSC测试链地址：0x74508F87ec6E34A148bD592Cf6700a38a56e89a4
 contract BSC_Defi {
 
     DemaxPlatform burgerSwap = DemaxPlatform(0x9FdC672a33f34675253041671abd214F2387b7aB);
@@ -38,19 +38,19 @@ contract BSC_Defi {
     BakerySwapRouter bakerySwap = BakerySwapRouter(0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F);
     BSCswapRouter bscSwap = BSCswapRouter(0xd954551853F55deb4Ae31407c423e67B1621424A);
     CheeseSwapRouter cheeseSwap = CheeseSwapRouter(0x3047799262d8D2EF41eD2a222205968bC9B0d895);
-    
+
     constructor( ) public {
-        
+
     }
 
     function query_prices(uint amountIn, address[] memory path) public returns(uint[] memory amounts ){
         amounts=new uint[](5);
-        
+
         amounts[0]=burgerSwap.getAmountsOut(amountIn,path)[1];
         amounts[1]=pancakeSwap.getAmountsOut(amountIn,path)[1];
         amounts[2]=bakerySwap.getAmountsOut(amountIn,path)[1];
         amounts[3]=bscSwap.getAmountsOut(amountIn,path)[1];
         amounts[4]=cheeseSwap.getAmountsOut(amountIn,path)[1];
     }
-    
+
 }
