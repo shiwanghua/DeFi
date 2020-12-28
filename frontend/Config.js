3888cb405268f6dec45ee85498cadf0ecb9c3ea8
Tantalus <jsl_713@live.com>
2020-12-28 19:22:09 +0800

    first upload

---
+++ frontend/Config.js
@@ -0,0 +1,12 @@
+
+var Address_MainContract = '';
+var HttpP="";
+var CoinAddress = new Map();
+CoinAddress.set("WBNB","0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c");CoinAddress.set("CAKE","0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82");
+CoinAddress.set("DAI","0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3");CoinAddress.set("BUGER","0xae9269f27437f0fcbc232d39ec814844a51d6b8f");
+CoinAddress.set("ETH","0x2170ed0880ac9a755fd29b2688956bd959f933f8");
+
+
+
+
+var ABI_MainContract = 
